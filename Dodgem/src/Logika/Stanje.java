5894d8fd3b2eb90989f8db13a82dbca9c06214bd
Katarina Koderman <koderman.katarina@gmail.com>
2018-05-03 10:54:52 +0200

    Ustvarila enum stanje.

---
+++ Dodgem/src/Logika/Stanje.java
@@ -0,0 +1,20 @@
+package Logika;
+
+public enum Stanje {
+	NA_POTEZI_X,
+	NA_POTEZI_Y,
+	ZMAGA_X,
+	ZMAGA_Y,;
+	
+	/**
+	 * Zmagovalni igralec ali null, èe ga ni
+	 */
+	
+/*	private Stanje() {zmagovalec = null;}
+	
+
+}
+	private LinkedList<Pozicije> pozicije(Igralec){
+		
+}*/
+}
