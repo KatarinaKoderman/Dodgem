9b156f484e7a0d5ef9d304270263e4cbdae8131e
Katarina Koderman <koderman.katarina@gmail.com>
2018-05-06 14:52:02 +0200

    Dodala razred Poteza.

---
+++ Dodgem/src/Logika/Poteza.java
@@ -0,0 +1,26 @@
+package Logika;
+
+public class Poteza {
+	private int x;
+	private int y;
+	private Smer smer;
+	
+	public Poteza(int x, int y, Smer smer) {
+		this.x = x;
+		this.y = y;
+		this.smer = smer;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public Smer getSmer() {
+		return smer;
+	}
+	
+}
