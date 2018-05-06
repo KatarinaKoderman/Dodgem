367b30ce530fae42fa3b98139f8a7bf8f29567f7
Katarina Koderman <koderman.katarina@gmail.com>
2018-05-06 17:52:08 +0200

    Razredu Igra dodala metodi poteze in stanje.
    Metoda poeteze ni dobro implementirana, saj niso ustrezno obravnavani
    primeri, ko avtomobil zapusti ploščo. Kako to dobro narediti?
    Metoda stanje je v "špageti stanju".
    Treba je napisati še metodo odigraj(Poteza p).

--- Dodgem/src/Logika/Igra.java
+++ Dodgem/src/Logika/Igra.java
@@ -1,11 +1,13 @@
 package Logika;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Igra {
 	/**
 	 * Velikost igralne plošèe N x N.
 	 */
 	public static final int N = 5;
-
 
 	/**
 	 * Atributi objekta iz razreda igra.
@@ -37,14 +39,94 @@ public class Igra {
 		naPotezi = Igralec.X;
 	}
 
-
 	/**
-	 * @return seznam možnih potez. LinkedList bo sestavljen iz 4-ih komponent: izvorno stanje (x, y) ter možna poteza, zopet z dvema koordinatama.
+	 * @return seznam možnih potez za X in Y glede na igralca, ki je na potezi
+	 * Pazi na orientacijo osi!
 	 */
-	
-/*		public List<Poteza> poteze() {
-			LinkedList<Poteza> ps = new LinkedList<Poteza>;
-			
-		}*/
 
+	public List<Poteza> poteze() {
+		LinkedList<Poteza> psX = new LinkedList<Poteza>();
+		LinkedList<Poteza> psY = new LinkedList<Poteza>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (plosca[i][j] == Polje.X) {
+					if (plosca[i-1][j] == Polje.PRAZNO && i>0) {
+						psX.add(new Poteza(i-1, j, Smer.DESNO));
+					}
+					if (plosca[i+1][j] == Polje.PRAZNO && i<N-1) {
+						psX.add(new Poteza(i+1, j, Smer.LEVO));
+					}
+					if (plosca[i][j+1] == Polje.PRAZNO) { 
+						// morda plosca[i][j+1] sploh ne obstaja! TODO
+						psX.add(new Poteza(i, j+1, Smer.NAPREJ));
+					}
+				}
+				if (plosca[i][j] == Polje.Y) {
+					if (plosca[i][j-1] == Polje.PRAZNO && j>0) {
+						psY.add(new Poteza(i, j-1, Smer.LEVO));
+					}
+					if (plosca[i][j+1] == Polje.PRAZNO && j<N-1) {
+						psY.add(new Poteza(i, j+1, Smer.DESNO));
+					}
+					if (plosca[i+1][j] == Polje.PRAZNO) {
+						// morda plosca[i+1][j] sploh ne obstaja! TODO
+						psY.add(new Poteza(i+1, j, Smer.NAPREJ));
+					}
+				}
+			}
+		}
+		if (naPotezi == Igralec.X) {
+			return psX;
+		}
+		else {	// na potezi je Y
+			return psY;
+		}
+	}
+	
+	/**
+	 * @return stanje igre
+	 */
+	public Stanje stanje() {
+		int steviloAvtomobilovX = 0;
+		int steviloAvtomobilovY = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) { 
+				//takoj, ko se eno od števil steviloAvtomobilov poveèa, ga ne rabimo veè šteti, naprej bi lahko pregledovali le za drugega igralca
+				if (plosca[i][j] == Polje.X) {
+					steviloAvtomobilovX += 1;
+				}
+				if (plosca[i][j] == Polje.Y) {
+					steviloAvtomobilovY +=1; 
+				}
+			}
+		}
+		if (steviloAvtomobilovX == 0) {
+			return Stanje.ZMAGA_X;
+		}
+		else {
+			if (steviloAvtomobilovY == 0) {
+				return Stanje.ZMAGA_Y;
+			}
+			else {
+				if (naPotezi == Igralec.X) {
+					return Stanje.NA_POTEZI_X;
+				}
+				else {
+					return Stanje.NA_POTEZI_Y;
+				}
+			}
+		}
+	}
+	
+/*
+	/**
+	 * Odigraj potezo p.
+	 * @param p
+	 * @return true, èe je bila poteza uspešno odigrana
+	 *//*
+	public boolean odigraj(Poteza p) {
+		
+	}
+
+*/
 }
