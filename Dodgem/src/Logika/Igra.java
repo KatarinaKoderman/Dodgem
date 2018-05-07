934b01ef8836184cdd09899365fa821f27cc1017
Matej Jazbec <jazbec.matej@outlook.com>
2018-05-07 23:44:28 +0200

    Add files via upload

--- Dodgem/src/Logika/Igra.java
+++ Dodgem/src/Logika/Igra.java
@@ -44,44 +44,49 @@ public class Igra {
 	 * Pazi na orientacijo osi!
 	 */
 
-	public List<Poteza> poteze() {
-		LinkedList<Poteza> psX = new LinkedList<Poteza>();
-		LinkedList<Poteza> psY = new LinkedList<Poteza>();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (plosca[i][j] == Polje.X) {
-					if (plosca[i-1][j] == Polje.PRAZNO && i>0) {
-						psX.add(new Poteza(i-1, j, Smer.DESNO));
-					}
-					if (plosca[i+1][j] == Polje.PRAZNO && i<N-1) {
-						psX.add(new Poteza(i+1, j, Smer.LEVO));
-					}
-					if (plosca[i][j+1] == Polje.PRAZNO) { 
-						// morda plosca[i][j+1] sploh ne obstaja! TODO
-						psX.add(new Poteza(i, j+1, Smer.NAPREJ));
-					}
-				}
-				if (plosca[i][j] == Polje.Y) {
-					if (plosca[i][j-1] == Polje.PRAZNO && j>0) {
-						psY.add(new Poteza(i, j-1, Smer.LEVO));
-					}
-					if (plosca[i][j+1] == Polje.PRAZNO && j<N-1) {
-						psY.add(new Poteza(i, j+1, Smer.DESNO));
-					}
-					if (plosca[i+1][j] == Polje.PRAZNO) {
-						// morda plosca[i+1][j] sploh ne obstaja! TODO
-						psY.add(new Poteza(i+1, j, Smer.NAPREJ));
-					}
-				}
-			}
-		}
-		if (naPotezi == Igralec.X) {
-			return psX;
-		}
-		else {	// na potezi je Y
-			return psY;
-		}
-	}
+    public List<Poteza> poteze() {
+        LinkedList<Poteza> psX = new LinkedList<Poteza>();
+        LinkedList<Poteza> psY = new LinkedList<Poteza>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (plosca[i][j] == Polje.X) {
+                    if (i > 0 && plosca[i-1][j] == Polje.PRAZNO) {
+                        psX.add(new Poteza(i-1, j, Smer.DESNO));
+                    }
+                    if (i<N-1 && plosca[i+1][j] == Polje.PRAZNO) {
+                        psX.add(new Poteza(i+1, j, Smer.LEVO));
+                    }
+                    if (j<N-1 && plosca[i][j+1] == Polje.PRAZNO) { 
+                        psX.add(new Poteza(i, j+1, Smer.NAPREJ));
+                    }
+                    if (j==N) {
+                    	psX.add(new Poteza(i, j+1, Smer.NAPREJ));
+                    }
+                }
+                if (plosca[i][j] == Polje.Y) {
+                    if (j>0 && plosca[i][j-1] == Polje.PRAZNO) {
+                        psY.add(new Poteza(i, j-1, Smer.LEVO));
+                    }
+                    if (j<N-1 && plosca[i][j+1] == Polje.PRAZNO) {
+                        psY.add(new Poteza(i, j+1, Smer.DESNO));
+                    }
+                    if (i<N-1 && plosca[i+1][j] == Polje.PRAZNO) {
+                        psY.add(new Poteza(i+1, j, Smer.NAPREJ));
+                    }
+                    if (i==N) {
+                    	psY.add(new Poteza(i+1, j, Smer.NAPREJ));
+                    }
+                }
+            }
+        }
+        if (naPotezi == Igralec.X) {
+            return psX;
+        }
+        else {    // na potezi je Y
+            return psY;
+        }
+    }
+
 	
 	/**
 	 * @return stanje igre
