520a149dca4d4ced56214024f1880c32deff75d5
Katarina Koderman <koderman.katarina@gmail.com>
2018-05-11 13:17:49 +0200

    Dodala smer ODSTRANI.
    Ko pride figura do roba plošče in jo lahko zapusti, ne more iti naprej,
    pač pa zapusti ploščo.

--- Dodgem/src/logika/Smer.java
+++ Dodgem/src/logika/Smer.java
@@ -3,13 +3,16 @@ package logika;
 public enum Smer {
 	LEVO,
 	DESNO,
-	NAPREJ;
+	NAPREJ,
+	ODSTRANI;
 
 	public String toString() {
 		switch (this) {
 		case LEVO: return "levo";
 		case DESNO: return "desno";
 		case NAPREJ: return "naprej";
+		case ODSTRANI: return "odstrani";
+		
 		default: return "?";
 		}
 	}
